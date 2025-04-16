c2e30f4bd4ff7771fcac2bdce9a4ff560385b598
hagee <hagee.ktr@outlook.com>
2025-04-16 08:12:44 +0300

    Generate code that throws an image of a dog using fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App () {
+    const [dogImage, setDogImage] = useState(null);
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then((response) => (response.json()))
+            .then((data) => setDogImage(data.message))
+            }, []);
+    
+        if (!dogImage) {
+            return <p>Loading...</p>;
+
+        }
+
+        return (
+        <div>
+          <img src={dogImage} alt="A Random Dog" />
+        </div>
+        )
+    }
+export default App;
